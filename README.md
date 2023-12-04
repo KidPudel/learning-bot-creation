f59a123fb039c7ceb9a5177a32b9cb328ec12696
Iggy Kuperson <i.kupchinenko@gmail.com>
2023-12-04 12:36:29 +0300

    finite state machine

--- README.md
+++ README.md
@@ -9,8 +9,10 @@ Dispatcher is the main router, they are needed for handling incoming updates fro
 # event handlers
 [event handlers](https://docs.aiogram.dev/en/dev-3.x/dispatcher/router.html)  
 [official docs](https://core.telegram.org/bots/api#august-18-2023)
-
-the order is metter
+## process of handling
+1. looking through current router (from outter -> nested) first is Dispatcher
+2. in router looking sequentially, from top to bottom (the first that satisfy filters (non-filtered handler is always the strongest))
+the order is matter
 
 # methods
 https://docs.aiogram.dev/en/dev-3.x/api/methods/index.html
@@ -18,3 +20,10 @@ https://docs.aiogram.dev/en/dev-3.x/api/methods/index.html
 # filters
 [filters](https://docs.aiogram.dev/en/dev-3.x/dispatcher/filters/command.html) are for events (updates) to be routed to the specific handler that match filter set.  
 Searching filters is always stops on first match set of filters are passed (empty filters are always passses, so **the order is matter**) . By defauilt all updates has empty filter set, so all updates will be passed to the handler that has no filters
+
+# state machine
+when you need to do some steps and determine on what bot message user is responding to
+
+This is done with a StateGroup, just define a states, to determine what step is now and what user must enter
+(like with enum group), just set state and then in a filters of a handler add needed state
+[fine-state-machine](https://docs.aiogram.dev/en/dev-3.x/dispatcher/finite_state_machine/index.html)
--- main.py
+++ main.py
@@ -1,33 +1,74 @@
+from enum import Enum
 import sys
 import os
 import asyncio
 import platform
 import logging
 
-from aiogram import Bot, Dispatcher, F
+
+from aiogram import Bot, Dispatcher, F, Router
 from aiogram.enums import ParseMode
 from aiogram.types import Message
 from aiogram.filters import Command
-
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 # to access bot
 TOKEN = os.environ.get("BOT_TOKEN")
 # to get updates from telegram
 dp = Dispatcher()
+
+my_router =Router(name="my_router")
+
+dp.include_router(my_router)
+
+
+class Review(StatesGroup):
+    name = State()
+    age = State()
+    rate = State()
+
+# ---------- flow -----------
+@my_router.message(F.text == "my router")
+async def my_handler(message: Message):
+    await message.answer(text="from my router")
+
 
 # that is dependency injection right there, decoupling objects, rather than using it directly
 @dp.message(Command("start", prefix="/"))
 async def start_handler(message: Message):
     await message.answer(text="Greetings in the Land of Ooo")
 
+
 # the order is matter, because it looks for filters it passes, but if no filters is there, then this handler will be picked
 @dp.message(lambda message: message.text == "test") # also could be done with F.text == "test"
 async def test_message_handler(message: Message):
     await message.answer(text="Test is completed successefully")
 
-@dp.message()
+@dp.message(F.text == "whoami")
 async def any_message_handler(message: Message):
     await message.answer(text=f"Your name is {message.chat.username} and this bot is running on {platform.system()}")
 
+@dp.message(Command("review", prefix="/"))
+async def start_review_handler(message: Message, state: FSMContext):
+    await state.set_state(Review.name)
+    await message.answer(text="what is your name?")
+
+# listen for the name
+@dp.message(Review.name)
+async def process_name_handler(message: Message, state: FSMContext):
+    await state.set_state(state=Review.age)
+    await message.answer(text=f"your name {message.text}")
+
+
+@dp.message(Review.age)
+async def process_age_handler(message: Message, state: FSMContext):
+    await state.set_state(state=Review.rate)
+    await message.answer(f"you are {message.text}yo")
+
+@dp.message(Review.rate)
+async def process_rate_handler(message: Message, state: FSMContext):
+    await state.clear()
+    await message.answer(text=f"you rated us with {message.text}, thx")
 
 @dp.edited_message()
 async def edited_message_handler(message: Message):
